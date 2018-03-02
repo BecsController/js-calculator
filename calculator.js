d5f4847062d6c06a82c2767a0d73d709ea8fec88
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-03 09:55:27 +1300

    java aaaarg

---
+++ calculator.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', start)
+
+function start() {
+    addEventListeners()
+}
+function addEventListeners() {
+
+var buttons = document.getElementsByClassName("buttons")
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", calculate)
+ }
+}
+ //add click event listener to each button in turn
+//so that when user clicks on button, display function is called.
+var temp = ''; // set up temp string for display
+var entries = [];  //set up empty array for calculation
+
+function calculate (evt) {
+  var val = evt.target.id  //add button text to value
+
+  if (val === 'C') {
+    temp = '';
+    entries = [];
+  }
+  if (val === 'squared') {
+    temp = (Math.sqrt(temp));
+    document.getElementById("answer").innerHTML = temp;
+  }
+  if (val === 'back') {
+    temp = temp.slice(0, - 1);
+    document.getElementById("answer").innerHTML = temp;
+  }
+
+  if (val === '=') {
+    var answer = eval(entries.join(' '));
+    var compute = answer;
+    document.getElementById("answer").innerHTML = compute;
+    temp = '';
+    entries = [];
+
+  } else {
+    temp += val;
+    document.getElementById("answer").innerHTML = temp;
+    entries.push(val);
+  }
+}
